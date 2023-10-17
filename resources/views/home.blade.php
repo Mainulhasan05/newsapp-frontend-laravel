e1d604e4074cf3a1c01012dcda3fe7c0d63e3412
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2023-10-17 23:44:00 +0600

    upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,17 @@
 {{-- extend master layout --}}
 @extends('layouts.master')
 
+    {{-- page title --}}
+    @section('title', 'News24')
+    {{-- page content --}}
+    @section('content')
+        <h1>Home</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    @endsection
+    {{-- page footer --}}
+    @section('footer')
+        @parent
+        <p>Home page footer</p>
+
+    
+@endsection
