614b85df419890bb531950c5972a6301c395ac29
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2023-11-13 22:57:42 +0600

    news count

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -55,37 +55,7 @@
                                 </div>
                                 <h1 class="mb-3 text-secondary text-uppercase font-weight-bold">{{$news->title}}</h1>
                                 <p>{!!$news->description!!}</p>
-                                <p>Voluptua est takimata stet invidunt sed rebum nonumy stet, clita aliquyam dolores
-                                    vero stet consetetur elitr takimata rebum sanctus. Sit sed accusam stet sit
-                                    nonumy kasd diam dolores, sanctus lorem kasd duo dolor dolor vero sit et. Labore
-                                    ipsum duo sanctus amet eos et. Consetetur no sed et aliquyam ipsum justo et,
-                                    clita lorem sit vero amet amet est dolor elitr, stet et no diam sit. Dolor erat
-                                    justo dolore sit invidunt.</p>
-                                <h3 class="text-uppercase font-weight-bold mb-3">Lorem ipsum dolor sit amet elit</h3>
-                                <img class="img-fluid w-50 float-left mr-4 mb-2" src="img/news-800x500-1.jpg">
-                                <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
-                                    invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
-                                    lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
-                                    rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
-                                    sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
-                                    lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
-                                    sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
-                                    dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
-                                    sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
-                                    duo tempor sea kasd clita ipsum et.</p>
-                                <h5 class="text-uppercase font-weight-bold mb-3">Lorem ipsum dolor sit amet elit</h5>
-                                <img class="img-fluid w-50 float-right mr-4 mb-2" src="img/news-800x500-2.jpg">
-                                <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
-                                    invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
-                                    lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
-                                    rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
-                                    sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
-                                    lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
-                                    sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
-                                    dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
-                                    sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
-                                    duo tempor sea kasd clita ipsum et. Takimata kasd diam justo est eos erat
-                                    aliquyam et ut.</p>
+                                
                             </div>
                             <div class="d-flex justify-content-between bg-white border border-top-0 p-4">
                                 <div class="d-flex align-items-center">
@@ -93,7 +63,7 @@
                                     <span>John Doe</span>
                                 </div>
                                 <div class="d-flex align-items-center">
-                                    <span class="ml-3"><i class="far fa-eye mr-2"></i>12345</span>
+                                    <span class="ml-3"><i class="far fa-eye mr-2"></i>{{$news->views}}</span>
                                     <span class="ml-3"><i class="far fa-comment mr-2"></i>123</span>
                                 </div>
                             </div>
