c4830d8c7fdaf1e145df0a891af496873a3fe693
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2023-11-13 22:36:23 +0600

    change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,9 +12,10 @@ class HomeController extends Controller
         $categories=Categories::all();
         // get last 5 news
         $latest_news=News::orderBy('id','desc')->take(5)->get();
-        // print $latest_news;
+        // get featured news 10
+        $featured_news=News::where('is_featured',1)->orderBy('id','desc')->take(10)->get();
  
         
-        return view("home",['categories'=>$categories,'latest_news'=>$latest_news]);
+        return view("home",['categories'=>$categories,'latest_news'=>$latest_news,'featured_news'=>$featured_news]);
     }
 }
