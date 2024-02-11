eebbc5d611882e50c87e0f40ced23343d95740c0
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-02-11 14:35:28 +0600

    description details page solved

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\News;
+use App\Models\Posts;
 
 class NewsController extends Controller
 {
     public function show($slug)
     {
-        $news = News::where('slug', $slug)->firstOrFail();
+        $news = Posts::where('slug', $slug)->firstOrFail();
         $news->increment('views');
         
-        $trending_news=News::orderBy('views','desc')->take(5)->get();
+        $trending_news=Posts::orderBy('views','desc')->take(5)->get();
         return view('news', compact('news','trending_news'));
     }
 }
